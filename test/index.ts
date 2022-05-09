487973add9094a5d3de6f239a858b5c131ee4b26
eternal4take666 <100616870+eternal4take666@users.noreply.github.com>
2022-05-09 10:54:58 +0300

    tests

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,149 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("NftMarketplaceTest", function () {
+  it("should create/sell nft", async function () {
+    const [owner, whitelist, user] = await ethers.getSigners();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const onlyOneToken = await ethers.getContractFactory("OnlyOne");
+    const OnlyOneToken = await onlyOneToken.deploy();
+    await OnlyOneToken.deployed();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const platform = await ethers.getContractFactory("Staking");
+    const Platform = await platform.deploy(OnlyOneToken.address);
+    await Platform.deployed();
+    
+    const nft = await ethers.getContractFactory("UndasGeneralNFT");
+    const Nft = await nft.deploy();
+    await Nft.deployed();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const nftMarketplace = await ethers.getContractFactory(
+      "Marketplace",
+      owner
+    );
+    const NftMarketplace = await nftMarketplace.deploy(
+      Platform.address,
+      OnlyOneToken.address,
+      whitelist.address,
+      20,
+      10
+    );
+    await NftMarketplace.deployed();
+    console.log("marketplace address :" + NftMarketplace.address);
+    console.log("erc20 deployed to" + OnlyOneToken.address);
+    console.log("platform deployed to" + Platform.address);
+    console.log("nft deployed to" + Nft.address);
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    await Nft.connect(user).safeMintGeneral(
+      user.address,
+      "test",
+      "test nft",
+      "https://img.png"
+    );
+    await Nft.connect(user).setApprovalForAll(NftMarketplace.address, true);
+    const value = ethers.utils.parseUnits("0.1", "ether");
+    expect(
+      await NftMarketplace.connect(user).bid(Nft.address, 0, "200", { 
+        value: value, 
+      })
+    ).to.emit(NftMarketplace, "Listed");
   });
+
+  it("should revert with error", async function () {
+    const [owner, whitelist, user] = await ethers.getSigners();
+
+    const onlyOneToken = await ethers.getContractFactory("OnlyOne");
+    const OnlyOneToken = await onlyOneToken.deploy();
+    await OnlyOneToken.deployed();
+
+    const platform = await ethers.getContractFactory("Staking");
+    const Platform = await platform.deploy(OnlyOneToken.address);
+    await Platform.deployed();
+
+    const nft = await ethers.getContractFactory("UndasGeneralNFT");
+    const Nft = await nft.deploy();
+    await Nft.deployed();
+
+    const nftMarketplace = await ethers.getContractFactory(
+      "Marketplace",
+      owner
+    );
+    const NftMarketplace = await nftMarketplace.deploy(
+      Platform.address,
+      OnlyOneToken.address,
+      whitelist.address,
+      20,
+      10
+    );
+    await NftMarketplace.deployed();
+    console.log("marketplace address :" + NftMarketplace.address);
+    console.log("erc20 deployed to" + OnlyOneToken.address);
+    console.log("platform deployed to" + Platform.address);
+    console.log("nft deployed to" + Nft.address);
+
+    await Nft.connect(user).safeMintGeneral(
+        user.address,
+        "test",
+        "test nft",
+        "https://img.png"
+    );
+
+    await Nft.connect(user).setApprovalForAll(NftMarketplace.address, true);
+    const value = ethers.utils.parseUnits("0.01", "ether");
+    await expect(
+        NftMarketplace.connect(user).bid(Nft.address, 0, "200", {
+          value: value,
+        })
+    ).to.be.revertedWith("bidFee");
+  });
+  it("should allow to buy nft", async function () {
+    const [owner, whitelist, user, buyer] = await ethers.getSigners();
+
+    const onlyOneToken = await ethers.getContractFactory("OnlyOne");
+    const OnlyOneToken = await onlyOneToken.deploy();
+    await OnlyOneToken.deployed();
+
+    const platform = await ethers.getContractFactory("Staking");
+    const Platform = await platform.deploy(OnlyOneToken.address);
+    await Platform.deployed();
+
+    const nft = await ethers.getContractFactory("UndasGeneralNFT");
+    const Nft = await nft.deploy();
+    await Nft.deployed();
+
+    const nftMarketplace = await ethers.getContractFactory(
+        "Marketplace",
+        owner
+    );
+    const NftMarketplace = await nftMarketplace.deploy(
+        Platform.address,
+        OnlyOneToken.address,
+        whitelist.address,
+        20,
+        10
+    );
+    await NftMarketplace.deployed();
+    console.log("marketplace address :" + NftMarketplace.address);
+    console.log("erc20 deployed to" + OnlyOneToken.address);
+    console.log("platform deployed to" + Platform.address);
+    console.log("nft deployed to" + Nft.address);
+
+    await Nft.connect(user).safeMintGeneral(
+        user.address,
+        "test",
+        "test nft",
+        "https://img.png"
+    );
+    await Nft.connect(user).setApprovalForAll(NftMarketplace.address, true);
+    const value = ethers.utils.parseUnits("0.1", "ether");
+
+    await NftMarketplace.connect(user).bid(Nft.address, 0, "200", {
+      value: value,
+    });
+    expect(
+        await NftMarketplace.connect(buyer).buyToken(0, { value: "200" })
+    ).changeEtherBalances([buyer, user], [-200, 200]);
+  });
+  // setApproval bug
+  // totalSupply bug
 });
